812c66144927bd4c70cc05367616560cfc58db72
Rodrigo Saraiva <rodrigobezerrasaraiva@gmail.com>
2023-12-03 17:02:06 -0300

    style: formmating with prettier

--- src/App.vue
+++ src/App.vue
@@ -14,7 +14,7 @@ const cardForm = ref<CardForm>({
   cardNumber: '',
   expDateMonth: '',
   expDateYear: '',
-  cvc: ''
+  cvc: '',
 });
 
 const submitForm = (formData: CardForm) => {
@@ -39,7 +39,9 @@ const submitForm = (formData: CardForm) => {
             class="absolute h-fit w-fit top-4 left-6"
             src="/assets/images/card-logo.svg"
           />
-          <span class="text-white text-2xl absolute font-light bottom-16 left-6">
+          <span
+            class="text-white text-2xl absolute font-light bottom-16 left-6"
+          >
             0000 0000 0000 0000
           </span>
           <span class="text-white absolute font-light text-sm bottom-4 left-6">
@@ -67,33 +69,65 @@ const submitForm = (formData: CardForm) => {
       id="card-form"
       class="flex flex-col justify-around place-items-center h-full w-1/2"
     >
-      <form @submit.prevent="submitForm(cardForm)" class="flex flex-col gap-6 items-center justify-center h-5/6 w-3/4 text-[#21092f]">
+      <form
+        @submit.prevent="submitForm(cardForm)"
+        class="flex flex-col gap-6 items-center justify-center h-5/6 w-3/4 text-[#21092f]"
+      >
         <section class="flex flex-col w-4/5 gap-1" id="card-field-name">
           <label for="card-name">CARDHOLDER NAME</label>
-          <input id="card-name" class="border-2 border-[#dedddf] rounded-md py-2 px-2" v-model="cardForm.name" placeholder="e.g. Jane Appleseed">
+          <input
+            id="card-name"
+            class="border-2 border-[#dedddf] rounded-md py-2 px-2"
+            v-model="cardForm.name"
+            placeholder="e.g. Jane Appleseed"
+          />
         </section>
         <section class="flex flex-col w-4/5 gap-1" id="card-field-number">
           <label for="card-number">CARD NUMBER</label>
-          <input id="card-number" class="border-2 border-[#dedddf] rounded-md py-2 px-2" v-model="cardForm.cardNumber" placeholder="e.g. 1234 5678 9123 0000">
+          <input
+            id="card-number"
+            class="border-2 border-[#dedddf] rounded-md py-2 px-2"
+            v-model="cardForm.cardNumber"
+            placeholder="e.g. 1234 5678 9123 0000"
+          />
         </section>
         <div class="flex flex-row w-4/5 gap-4">
           <section class="flex flex-col w-1/2 gap-1" id="card-field-exp-date">
             <label for="card-exp-date">EXP. DATE (MM/YY)</label>
             <div class="flex gap-2">
-              <input class="border-2 border-[#dedddf] rounded-md w-3/5 px-3 py-2" id="card-exp-date-month" v-model="cardForm.expDateMonth" placeholder="MM">
-              <input class="border-2 border-[#dedddf] rounded-md w-3/5 px-3 py-2" id="card-exp-date-year" v-model="cardForm.expDateYear" placeholder="YY">
+              <input
+                class="border-2 border-[#dedddf] rounded-md w-3/5 px-3 py-2"
+                id="card-exp-date-month"
+                v-model="cardForm.expDateMonth"
+                placeholder="MM"
+              />
+              <input
+                class="border-2 border-[#dedddf] rounded-md w-3/5 px-3 py-2"
+                id="card-exp-date-year"
+                v-model="cardForm.expDateYear"
+                placeholder="YY"
+              />
             </div>
-          </section>        
+          </section>
           <section class="flex flex-col w-1/2 gap-1" id="card-field-cvc">
             <label for="card-cvc">CVC</label>
-            <input class="border-2 border-[#dedddf] rounded-md py-2 px-3" id="card-cvc" v-model="cardForm.cvc" placeholder="e.g. 123">
+            <input
+              class="border-2 border-[#dedddf] rounded-md py-2 px-3"
+              id="card-cvc"
+              v-model="cardForm.cvc"
+              placeholder="e.g. 123"
+            />
           </section>
         </div>
-        <button class="w-4/5 py-4 mt-8 border-2 rounded-md bg-[#21092f] text-white" type="submit">Confirm</button>
+        <button
+          class="w-4/5 py-4 mt-8 border-2 rounded-md bg-[#21092f] text-white"
+          type="submit"
+        >
+          Confirm
+        </button>
       </form>
     </section>
   </main>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
