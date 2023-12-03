29f541689c333ce2c7f67513a638b7213e4369a4
Rodrigo Saraiva <rodrigobezerrasaraiva@gmail.com>
2023-12-03 10:39:51 -0300

    feat: aligning exp-date and cvc fields + styling submit button

--- src/App.vue
+++ src/App.vue
@@ -23,7 +23,7 @@ const submitForm = (formData: CardForm) => {
 </script>
 
 <template>
-  <main class="flex border-4 border-yellow-500">
+  <main class="flex">
     <section
       id="card-faces"
       class="flex flex-col justify-around place-items-center border-2 border-blue-700 h-full w-1/2"
@@ -43,25 +43,29 @@ const submitForm = (formData: CardForm) => {
       id="card-form"
       class="flex flex-col justify-around place-items-center border-2 border-red-700 h-full w-1/2"
     >
-      <form @submit.prevent="submitForm(cardForm)" class="flex flex-col gap-6 items-center justify-center border-2 border-green-700 h-5/6 w-3/4 text-[#21092f]">
+      <form @submit.prevent="submitForm(cardForm)" class="flex flex-col gap-6 items-center justify-center h-5/6 w-3/4 text-[#21092f]">
         <section class="flex flex-col w-4/5 gap-1" id="card-field-name">
           <label for="card-name">CARDHOLDER NAME</label>
           <input id="card-name" class="border-2 border-[#dedddf] rounded-md py-2 px-2" v-model="cardForm.name" placeholder="e.g. Jane Appleseed">
         </section>
         <section class="flex flex-col w-4/5 gap-1" id="card-field-number">
-          <label for="card-number">CARDHOLDER NUMBER</label>
+          <label for="card-number">CARD NUMBER</label>
           <input id="card-number" class="border-2 border-[#dedddf] rounded-md py-2 px-2" v-model="cardForm.cardNumber" placeholder="e.g. 1234 5678 9123 0000">
         </section>
-        <section class="flex flex-col w-4/5 gap-1" card="card-field-exp-date">
-          <label for="card-exp-date">EXP. DATE (MM/YY)</label>
-          <input class="border-2 border-[#dedddf] rounded-md py-2 px-2" id="card-exp-date-month" v-model="cardForm.expDateMonth">
-          <input class="border-2 border-[#dedddf] rounded-md py-2 px-2" id="card-exp-date-year" v-model="cardForm.expDateYear">
-        </section>
-        <section class="flex flex-col w-4/5 gap-1" id="card-field-cvc">
-          <label for="card-cvc">CVC</label>
-          <input class="border-2 border-[#dedddf] rounded-md py-2 px-2" id="card-cvc" v-model="cardForm.cvc">
-        </section>
-        <button class="border-2 rounded-md bg-[#21092f] text-white py-4 px-24" type="submit">Confirm</button>
+        <div class="flex flex-row w-4/5 gap-4">
+          <section class="flex flex-col w-1/2 gap-1" id="card-field-exp-date">
+            <label for="card-exp-date">EXP. DATE (MM/YY)</label>
+            <div class="flex gap-2">
+              <input class="border-2 border-[#dedddf] rounded-md w-3/5 px-3 py-2" id="card-exp-date-month" v-model="cardForm.expDateMonth" placeholder="MM">
+              <input class="border-2 border-[#dedddf] rounded-md w-3/5 px-3 py-2" id="card-exp-date-year" v-model="cardForm.expDateYear" placeholder="YY">
+            </div>
+          </section>        
+          <section class="flex flex-col w-1/2 gap-1" id="card-field-cvc">
+            <label for="card-cvc">CVC</label>
+            <input class="border-2 border-[#dedddf] rounded-md py-2 px-3" id="card-cvc" v-model="cardForm.cvc">
+          </section>
+        </div>
+        <button class="w-4/5 py-4 border-2 rounded-md bg-[#21092f] text-white" type="submit">Confirm</button>
       </form>
     </section>
   </main>
